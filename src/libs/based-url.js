81fba68bb092201e8ab8f0c82d55a6ca5ab70748
Sitesh Kumar <siteshkjha9@gmail.com>
2026-03-28 19:55:13 +0530

    update the base backend url

--- src/libs/based-url.js
+++ src/libs/based-url.js
@@ -2,5 +2,5 @@
 export const basedUrl = "https://realestate123.pythonanywhere.com"
 
 // New Base URL - For new API endpoints
-export const newBasedUrl = "https://finaltrs.onrender.com"
+export const newBasedUrl = "https://trs-api.trspropertymall.com"
 
